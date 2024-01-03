c3500240350b9896c72bfc1ef4e44a15d9f48dd1
hrnxm <harun.muderizovic@gmail.com>
2024-01-03 20:14:13 +0100

    2024/01/03, Fix Inter font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,14 +6,28 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 };
 
