f80542a2066d45a6f3169acf02d7deb596e40cee
hrnxm <harun.muderizovic@gmail.com>
2024-01-14 21:14:25 +0100

    2024/01/14, Implement post creation on the server

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,12 +1,36 @@
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "~/server/api/trpc";
+import isMorseCode from "~/utils/isMorseCode";
 
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.db.post.findMany({
       include: { user: true },
       orderBy: { createdAt: "desc" },
+    });
+  }),
+  create: protectedProcedure
+    .input(
+      z.object({
+        content: z
+          .string()
+          .min(1, "Post is empty!")
+          .max(750, "Character limit is 750!")
+          .refine(isMorseCode, { message: "Post must be in Morse code!" }),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const userId = ctx.session.user.id;
+      return ctx.db.post.create({
+        data: {
+          userId,
+          content: input.content,
+        },
       });
     }),
 });
